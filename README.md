3efd9b13b0969f9aeb2cc8fe2e042ba46ebe28bc
lawrencejews <lawrencejews1@gmail.com>
2022-12-05 14:05:21 +0900

    added .env,DBAPI for sql-05-12-2022

--- README.md
+++ README.md
@@ -8,3 +8,4 @@
 - Start the server with uvicorn: `uvicorn app.main:app --reload`.
 - The pydantic library is used for data validation when creating schemas in a python application.
 - Create a python package use `__init__.py` in the folder of choose.
+- Psycopg driver for PostgresSQL to implement a complete Python DB API NOT for windows  `sudo apt install python3-dev libpq-dev`then `pip install psycopg2`.
--- app/main.py
+++ app/main.py
@@ -1,18 +1,47 @@
 from typing import Optional
-from fastapi import FastAPI, Response,status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
+import psycopg2
+from psycopg2.extras import RealDictCursor
 
+import os
+import time
+from dotenv import load_dotenv
 
+# Create environment variables for the database.
+load_dotenv(dotenv_path='.env')
+
+FASTAPI_SERVER = os.getenv('FASTAPI_SERVER')
+FASTAPI_DATABASE = os.getenv('FASTAPI_DATABASE')
+FASTAPI_USER = os.getenv('FASTAPI_USER')
+FASTAPI_PASSWORD = os.getenv('FASTAPI_PASSWORD')
+
+# Instance of fastapi
 app = FastAPI()
+
 
 # Created a schema
 class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+
+
+# Connection to the database
+while True:
+
+    try:
+        conn = psycopg2.connect(host=FASTAPI_SERVER, database=FASTAPI_DATABASE,
+                                user=FASTAPI_USER, password=FASTAPI_PASSWORD, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfull!")
+        break
+    except Exception as error:
+        print("Connection to database failed")
+        print('Error', error)
+        time.sleep(2)
 
 
 # Store posts objects
@@ -29,9 +58,9 @@ def find_post(id):
 
 # Find the index
 def find_index_post(id):
-  for i, p in enumerate(my_posts):
-    if p['id'] == id:
-      return i
+    for i, p in enumerate(my_posts):
+        if p['id'] == id:
+            return i
 
 
 # API requests
@@ -42,22 +71,27 @@ def root():
 
 @app.get('/posts')
 def get_posts():
-    return {"data": my_posts}
+    cursor.execute(""" SELECT  * FROM posts """)
+    posts = cursor.fetchall()
+    print(posts)
+    return {"data": posts}
 
 
-@app.post("/posts", status_code=status.HTTP_201_CREATED )
+@app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    # pydantic dictionary
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0, 1000000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    cursor.execute(""" INSERT INTO posts (title, content, published) VALUES(%s, %s, %s) RETURNING * """,
+                   (post.title, post.content, post.published))
+    new_post = cursor.fetchone()
+
+    conn.commit()
+
+    return {"data": new_post}
 
 
 @app.get('/posts/{id}')
 def get_post(id: int):
-    post = find_post(id)
-
+    cursor.execute(""" SELECT * FROM posts WHERE id= %s """, (str(id),))
+    post = cursor.fetchone()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id: {id} was not found")
@@ -68,27 +102,27 @@ def get_post(id: int):
 # Delete post - status code in fastapi returns a response.
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-  index = find_index_post(id)
+    cursor.execute(
+        """ DELETE FROM posts WHERE id= %s RETURNING * """, (str(id),))
+    delete_post = cursor.fetchone()
+    conn.commit()
+    if delete_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist ")
 
-  if index == None:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                       detail=f"post with id: {id} does not exist ")
-
-  my_posts.pop(index)
-  return Response(status_code=status.HTTP_204_NO_CONTENT)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 # Update posts
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
+    cursor.execute(""" UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING * """,
+                   (post.title, post.content, post.published, str(id)))
+    updated_post = cursor.fetchone()
+    conn.commit()
 
-  index = find_index_post(id)
+    if updated_post == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"post with id: {id} does not exist ")
 
-  if index == None:
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                       detail=f"post with id: {id} does not exist ")
-
-  post_dict = post.dict()
-  post_dict['id'] = id
-  my_posts[index] = post_dict
-  return {"data": post_dict}
+    return {"data": updated_post}
