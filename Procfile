04920b3d3d38bd423b43a70ddcc2d98b52cf6394
lawrencejews <lawrencejews1@gmail.com>
2023-02-15 17:41:13 +0900

    added a Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: uvicorn app.mainnosql:app --host=0.0.0.0 --port=${PORT:-5000}
--- app/mainnosql.py
+++ app/mainnosql.py
@@ -24,3 +24,7 @@ app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(vote.router)
+
+app.get("/")
+def root():
+    return {"message": "Connected to Heroku"}
